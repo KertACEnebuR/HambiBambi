50c6ce45486cc1cf63f6b437ac3dccdbbaf579bb
Kertész Ruben <70895461+KertACEnebuR@users.noreply.github.com>
2025-03-06 13:11:00 +0100

    Add files via upload

---
+++ hambibambi/application/view/menu/insideNavbar.php
@@ -0,0 +1,12 @@
+<div class="insideNavbar">
+    <nav id = "navbar">
+        <a class ="nav-link" href ="#hamburgers">Hamburgerek</a>
+        <a class ="nav-link" href="#soups">Levesek</a>
+        <a class ="nav-link" href="#drinks">Italok</a>
+        <a class ="nav-link" href="#salads">Saláták</a>
+        <a class ="nav-link" href="#pastas">Tészták</a>
+        <a class ="nav-link" href="#desserts">Sütik</a>
+        <a class ="nav-link" href="#drinks">Üdítők</a>
+        <a class ="nav-link" href="#drinks2">Alkoholos italok</a>
+    </nav>
+</div>
--- hambibambi/application/view/menu/menu.php
+++ hambibambi/application/view/menu/menu.php
@@ -3,23 +3,11 @@ $current_tab = isset($_GET['tab']) ? $_GET['tab'] : 'login';
 ?>
 
 <?php include("header.php"); ?>
-<?php include("../navbar/navbar.php"); ?>
-    <div class="insideNavbar">
-        <nav id = "navbar">
-            <a class ="nav-link" href ="#hamburgers">Hamburgerek</a>
-            <a class ="nav-link" href="#soups">Levesek</a>
-            <a class ="nav-link" href="#drinks">Italok</a>
-            <a class ="nav-link" href="#salads">Saláták</a>
-            <a class ="nav-link" href="#pastas">Tészták</a>
-            <a class ="nav-link" href="#desserts">Sütik</a>
-            <a class ="nav-link" href="#drinks">Üdítők</a>
-            <a class ="nav-link" href="#drinks2">Szeszes italok</a>
-        </nav>
-    </div>
-    <main>
 
-        <?php require $Path . "application/view/product.html"; ?>
-    </p>
-   
-    </main>
+<?php include("../navbar/navbar.php"); ?>
+
+<?php include("insideNavbar.php"); ?>
+
+<?php require $Path . "application/view/product.html"; ?> 
+
 <?php include("../footer.php"); ?>
