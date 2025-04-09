6b81905915bde958a9cb2a5c0d3eade7517ac794
Kertész Ruben <70895461+KertACEnebuR@users.noreply.github.com>
2025-04-09 13:49:39 +0200

    Add files via upload

--- hambibambi/assets/js/loginreg.js
+++ hambibambi/assets/js/loginreg.js
@@ -1,31 +1,24 @@
 function sendLogin (e) {
   console.log(e);
-  //e.preventDefault();
-  //alert("sendlogin");
 
   let email    = document.querySelector(".email").value;
   let password = document.querySelector(".password").value;
 
-  // Your JSON data
   const jsonData = { c: 'login', email:email, password:password };
 
-  // Set up options for the fetch request
   const options = {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json' // Set content type to JSON
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(jsonData) // Convert JSON data to a string and set it as the request body
+    body: JSON.stringify(jsonData)
   };
 
-  // Make the fetch request with the provided options
   fetch('../../controller/loginController.php', options)
     .then(response => {
-      // Check if the request was successful
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      // Parse the response as JSON
       return response.json();
     })
     .then(data => {
@@ -41,7 +34,6 @@ function sendLogin (e) {
       }
   })
   .catch(error => {
-    // Handle any errors that occurred during the fetch
     console.error('Fetch error:', error);
   });    
 }
